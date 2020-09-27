9b6c4f6b428f92635f356a71cb46a8de7e992694
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2020-09-28 01:50:25 +0200

    failed attempt to package
    I give up. With the current structure, it is not possible to build a
    clingo wheel. The project would have to be restructured and I am not
    going to do this in the foreseeable future.

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 import sys
 import site
+from setuptools import find_packages
 from skbuild import setup
 
-site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+if not site.ENABLE_USER_SITE and "--user" in sys.argv[1:]:
+    site.ENABLE_USER_SITE = True
 
 setup(
     version = '5.5.0',
@@ -11,4 +13,5 @@ setup(
     author = 'Roland Kaminski',
     license = 'MIT',
     cmake_args=['-DPYCLINGO_USER_INSTALL=OFF', '-DPYCLINGO_USE_INSTALL_PREFIX=ON', '-DPYCLINGO_INSTALL_DIR=.'],
+    packages=['clingo']
 )
