629182812feb0b9ecfc04736d6bc1bead1022c79
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2020-09-28 00:02:38 +0200

    tweak setup.py to install in the right place

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
+import sys
+import site
 from skbuild import setup
+
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
 setup(
     version = '5.5.0',
@@ -6,5 +10,5 @@ setup(
     description = 'A grounder and solver for logic programs.',
     author = 'Roland Kaminski',
     license = 'MIT',
-    cmake_args=['-DPYCLINGO_USER_INSTALL=OFF', '-DPYCLINGO_USE_INSTALL_PREFIX=ON'],
+    cmake_args=['-DPYCLINGO_USER_INSTALL=OFF', '-DPYCLINGO_USE_INSTALL_PREFIX=ON', '-DPYCLINGO_INSTALL_DIR=.'],
 )
